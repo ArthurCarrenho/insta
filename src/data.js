5afbaf786d5caaed62c10776c1c07dfce45841f9
Arthur Carrenho <arthur.carrenho@unifacef.com.br>
2022-06-15 14:02:46 -0300

    uwu

---
+++ src/data.js
@@ -0,0 +1,130 @@
+export const dataAPI = {
+  userLoggedInfo: {
+    name: "arthurcarrenho",
+    profile_image: require("../assets/images/profile.jpeg"),
+  },
+
+  stories: [
+    {
+      name: "Seu Story",
+      id: 32093,
+      profile_image: require("../assets/images/profile.jpeg"),
+    },
+    {
+      name: "dcunifacef",
+      id: 33425,
+      profile_image: {
+        uri: "https://instagram.frao1-2.fna.fbcdn.net/v/t51.2885-19/65312755_384989035478760_5271726443468947456_n.jpg?stp=dst-jpg_s320x320&_nc_ht=instagram.frao1-2.fna.fbcdn.net&_nc_cat=100&_nc_ohc=FA4XkkgZI7EAX8-z94c&tn=iYJG1dhgTexF_dvh&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AT_e1oUMyoNNUTsGs2l0XN8pejzDMdLwqnwegCbFL4QuOw&oe=62B02113&_nc_sid=8fd12b",
+      },
+    },
+    {
+      name: "atleticafacef",
+      id: 45235,
+      profile_image: {
+        uri: "https://instagram.ffrc6-1.fna.fbcdn.net/v/t51.2885-19/284971819_130297502732605_5508961572803569319_n.jpg?stp=dst-jpg_s150x150&_nc_ht=instagram.ffrc6-1.fna.fbcdn.net&_nc_cat=101&_nc_ohc=J1F8oEhR7OAAX-bLI4E&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AT86HRiQdhQpIqzl57hb_JFBNQMj68Q3Q9_hvWCfwkNKKg&oe=62AD7CCE&_nc_sid=8fd12b",
+      },
+    },
+    {
+      name: "casimiro",
+      id: 23546,
+      profile_image: {
+        uri: "https://instagram.frao1-2.fna.fbcdn.net/v/t51.2885-19/269652772_1604081053263013_733365440540750963_n.jpg?stp=dst-jpg_s320x320&_nc_ht=instagram.frao1-2.fna.fbcdn.net&_nc_cat=1&_nc_ohc=EycQBFz4LoYAX9m0S0c&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AT-Uh1irHXtCnffE7TdUhwuujk3FP4wXYlCg6B0ZExageg&oe=62AF70B3&_nc_sid=8fd12b",
+      },
+    },
+    {
+      name: "oticket.com.br",
+      id: 32803,
+      profile_image: {
+        uri: "https://instagram.frao1-1.fna.fbcdn.net/v/t51.2885-19/273410979_3179424838953150_4636877244247017170_n.jpg?stp=dst-jpg_s320x320&_nc_ht=instagram.frao1-1.fna.fbcdn.net&_nc_cat=106&_nc_ohc=4pQWw1J15YoAX-Qqsgq&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AT-O5G7qdTG3rBejH4nq-HRnuK6LVU5e3Mdz0-EfAa350A&oe=62AF2915&_nc_sid=8fd12b",
+      },
+    },
+  ],
+  posts: [
+    {
+      username: "casimiro",
+      id: 3457,
+      url: {
+        uri: "https://instagram.frao1-2.fna.fbcdn.net/v/t51.2885-15/287493367_102297682498480_6728223688715519801_n.jpg?stp=dst-jpg_e35_s640x640_sh0.08&_nc_ht=instagram.frao1-2.fna.fbcdn.net&_nc_cat=1&_nc_ohc=pT-XDiVYnZIAX9P__sr&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AT_hG3lf7gbRov9QaSQ7QYu2FXmcO1dfJdWIxZZyB7lUHg&oe=62AF5EE5&_nc_sid=8fd12b",
+      },
+      user_profile_photo: {
+        uri: "https://instagram.frao1-2.fna.fbcdn.net/v/t51.2885-19/269652772_1604081053263013_733365440540750963_n.jpg?stp=dst-jpg_s320x320&_nc_ht=instagram.frao1-2.fna.fbcdn.net&_nc_cat=1&_nc_ohc=EycQBFz4LoYAX9m0S0c&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AT-Uh1irHXtCnffE7TdUhwuujk3FP4wXYlCg6B0ZExageg&oe=62AF70B3&_nc_sid=8fd12b",
+      },
+      carrousel: false,
+      location: "Nba house",
+      likes: "158",
+      commonLike: {
+        name: "oticket.com.br",
+        profilePhoto: {
+          uri: "https://instagram.frao1-1.fna.fbcdn.net/v/t51.2885-19/273410979_3179424838953150_4636877244247017170_n.jpg?stp=dst-jpg_s320x320&_nc_ht=instagram.frao1-1.fna.fbcdn.net&_nc_cat=106&_nc_ohc=4pQWw1J15YoAX-Qqsgq&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AT-O5G7qdTG3rBejH4nq-HRnuK6LVU5e3Mdz0-EfAa350A&oe=62AF2915&_nc_sid=8fd12b",
+        },
+      },
+      description: "oi, @nbabrasil 🏀",
+      comments: "89",
+      time: "30 minutes ago",
+    },
+    {
+      username: "dcunifacef",
+      id: 2345,
+      url: [
+        {
+          uri: "https://instagram.frao1-2.fna.fbcdn.net/v/t51.2885-15/283648990_3161820300740007_2637184769728421448_n.webp?stp=dst-jpg_e35_s1080x1080&_nc_ht=instagram.frao1-2.fna.fbcdn.net&_nc_cat=103&_nc_ohc=Z3yQ-YbKPlYAX--pBvO&tn=iYJG1dhgTexF_dvh&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AT-4L5PI19HcWC4OpZsG_2oQa_PyVNXaGZIglUmxXn3f4g&oe=62B01629&_nc_sid=8fd12b",
+        },
+        {
+          uri: "https://instagram.frao1-2.fna.fbcdn.net/v/t51.2885-15/278419287_110163298220127_4611317217839248034_n.webp?stp=c180.0.1080.1080a_dst-jpg_e35_s640x640_sh0.08&_nc_ht=instagram.frao1-2.fna.fbcdn.net&_nc_cat=103&_nc_ohc=2ImcWnax1A4AX-flMfY&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AT92XhFnU7JsqKK1diE46ic1vD430OPAV1j9K5gHhEJFxw&oe=62AF8890&_nc_sid=8fd12b",
+        },
+        {
+          uri: "https://instagram.frao1-1.fna.fbcdn.net/v/t51.2885-15/278211852_7217280531676590_3935340386125686814_n.webp?stp=c180.0.1080.1080a_dst-jpg_e35_s640x640_sh0.08&_nc_ht=instagram.frao1-1.fna.fbcdn.net&_nc_cat=106&_nc_ohc=TeCv9EBWYtQAX9CtpGh&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AT8KeK5Qme24OIiHWQPPFCctjtghr15qBcLs0KbZMfNSEQ&oe=62AF40B4&_nc_sid=8fd12b",
+        },
+      ],
+      user_profile_photo: {
+        uri: "https://instagram.frao1-2.fna.fbcdn.net/v/t51.2885-19/65312755_384989035478760_5271726443468947456_n.jpg?stp=dst-jpg_s320x320&_nc_ht=instagram.frao1-2.fna.fbcdn.net&_nc_cat=100&_nc_ohc=FA4XkkgZI7EAX8-z94c&tn=iYJG1dhgTexF_dvh&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AT_e1oUMyoNNUTsGs2l0XN8pejzDMdLwqnwegCbFL4QuOw&oe=62B02113&_nc_sid=8fd12b",
+      },
+      carrousel: true,
+      location: "Unifacef",
+      likes: "780",
+      commonLike: {
+        name: "atleticafacef",
+        profilePhoto: {
+          uri: "https://instagram.ffrc6-1.fna.fbcdn.net/v/t51.2885-19/284971819_130297502732605_5508961572803569319_n.jpg?stp=dst-jpg_s150x150&_nc_ht=instagram.ffrc6-1.fna.fbcdn.net&_nc_cat=101&_nc_ohc=J1F8oEhR7OAAX-bLI4E&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AT86HRiQdhQpIqzl57hb_JFBNQMj68Q3Q9_hvWCfwkNKKg&oe=62AD7CCE&_nc_sid=8fd12b",
+        },
+      },
+      description: "XII Semana de Computação - Palestra sobre Segurança da Informação",
+      comments: 134,
+      time: "45 minutes ago",
+    },
+    {
+      username: "diboneproducoes",
+      id: 5689,
+      url: {
+        uri: "https://instagram.ffrc6-1.fna.fbcdn.net/v/t51.2885-15/286709460_1994542517411033_1337450046814603097_n.jpg?stp=c0.690.1775.1775a_dst-jpg_e35_s640x640_sh0.08&_nc_ht=instagram.ffrc6-1.fna.fbcdn.net&_nc_cat=105&_nc_ohc=bEneV64zmAYAX-b2p4v&tn=iYJG1dhgTexF_dvh&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AT9WHdT5Mq8QIdItbWp2KiD83x8imkzQURm0DqQVBjwQ5w&oe=62A99F8C&_nc_sid=8fd12b",
+      },
+      user_profile_photo: {
+        uri: "https://instagram.ffrc6-1.fna.fbcdn.net/v/t51.2885-19/148525561_180263293878007_622342155193307038_n.jpg?stp=dst-jpg_s150x150&_nc_ht=instagram.ffrc6-1.fna.fbcdn.net&_nc_cat=107&_nc_ohc=MQpfrivTeZcAX8EyNyn&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AT-XGKOVQwbfZVzGiCNmCbJZ-zTdulNX16Me5yD9pqXjrg&oe=62AEB5BD&_nc_sid=8fd12b",
+      },
+      carrousel: false,
+      location: undefined,
+      likes: "1.400",
+      commonLike: undefined,
+      description: "O BATUCA vai ser ainda mais fod@ em 2022: 48 horas de insanidade e muuuito samba, tudo no mesmo lugar! 🤤🥁",
+      comments: "500",
+      time: "50 minutes ago",
+    },
+    {
+      username: "ligalup",
+      id: 7634,
+      url: {
+        uri: "https://instagram.ffrc6-1.fna.fbcdn.net/v/t51.2885-15/286433220_133505212635938_3976733774578682435_n.jpg?stp=dst-jpg_e35_s640x640_sh0.08&_nc_ht=instagram.ffrc6-1.fna.fbcdn.net&_nc_cat=102&_nc_ohc=OzyEI_Tv3mMAX-RSF8E&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AT8Hk0iCpHL7-QXs8fhKya-pHR9gLyQMO_kUCVWgbL5d9g&oe=62AF282A&_nc_sid=8fd12b",
+      },
+      user_profile_photo: {
+        uri: "https://instagram.ffrc6-1.fna.fbcdn.net/v/t51.2885-19/242845832_385609466555051_527014900726073300_n.jpg?stp=dst-jpg_s150x150&_nc_ht=instagram.ffrc6-1.fna.fbcdn.net&_nc_cat=111&_nc_ohc=bJDF3LFDYgMAX90uoch&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AT-Dh7nsFECjKNaPGwDFmkWzHmdnS4lCD1TY8uWJ3-A1pw&oe=62AE13E8&_nc_sid=8fd12b",
+      },
+      carrousel: false,
+      location: undefined,
+      likes: "6.123",
+      commonLike: undefined,
+      description: "A arena de domingo entrou pra história e no link da bio tem todos os cliques da maior LUP de todos os tempos! 📸",
+      comments: "1.637",
+      time: "22 minutes ago",
+    },
+  ],
+};
